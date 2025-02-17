14b7d02c7b7f232acb7b1ed3dae3f66585c3eb67
David Rivard <55965524+Dave-21@users.noreply.github.com>
2025-02-17 09:33:14 -0500

    Create wheather_check.py

---
+++ wheather_check.py
@@ -0,0 +1,11 @@
+## wheather_check.py
+
+"""
+Checks weather conditions for observation viability.
+
+TODO:
+- Fetch cloud cover data from Open-Meteo API.
+- Fetch moon phase data to determine light interference.
+- Decide whether conditions are optimal for imaging.
+- Fetch rain/snow percentage.
+"""
