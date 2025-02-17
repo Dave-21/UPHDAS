b35b9685c6b6059f46d6a9cb7e42a40a0a7e0274
David Rivard <55965524+Dave-21@users.noreply.github.com>
2025-02-17 09:33:59 -0500

    Create update_tle_catalog.py

---
+++ update_tle_catalog.py
@@ -0,0 +1,9 @@
+## update_tle_catalog.py
+
+"""
+Fetches the latest satellite TLE data.
+
+TODO:
+- Pull TLEs from space-track.org and Celestrak (for the most part).
+- Save updated TLEs in the project directory.
+"""
