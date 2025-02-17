ff69059b3c56c161e48082b55d58e08986c410cb
David Rivard <55965524+Dave-21@users.noreply.github.com>
2025-02-17 09:32:00 -0500

    Create extract_ra_dec.py

---
+++ extract_ra_dec.py
@@ -0,0 +1,9 @@
+## extract_ra_dec.py
+
+"""
+Extracts RA/Dec from streaks using plate solution.
+
+TODO:
+- Convert pixel coordinates of detected streaks to celestial coordinates.
+- Verify extracted data against known star catalog.
+"""
