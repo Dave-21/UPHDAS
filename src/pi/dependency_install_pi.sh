285ebab69763c5b6b09d09712d9fd59eb3d6f247
dudeman36012 <117118887+dudeman36012@users.noreply.github.com>
2025-02-19 09:45:47 -0500

    Update dependency_install_pi.sh

--- src/pi/dependency_install_pi.sh
+++ src/pi/dependency_install_pi.sh
@@ -8,6 +8,7 @@ echo "Installing Python3, pip, and git..."
 sudo apt install -y python3 python3-pip git
 
 echo "Installing Python packages..."
+#using pip3 for installation doesn't work on linux, try install python3-[package]
 pip3 install -r ../../requirements.txt
 
 echo "Pi dependencies installed."
