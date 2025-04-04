6509a099c1c1ac2101e6a729076d92da9b38cbc7
dudeman36012 <117118887+dudeman36012@users.noreply.github.com>
2025-04-04 12:00:24 -0400

    Added error checking

--- src/pi/calibrate_image.py
+++ src/pi/calibrate_image.py
@@ -1,49 +1,81 @@
 import numpy as np
 import cv2 as cv
+import yaml
+import get_config as gc
+import error_Handler as ERR
+import os
 
-#Disclaimer: some code used from opencv website
+def undistort_Image(filepath):
+    #Disclaimer: some code used from opencv website
 
-#File to load calibration data from
-paramFile = 'calibrationParams.npz'
+    cfg = gc.load_config().get("image_calibration")
 
-#File to save calibrated image to.
-#Index 0 is the filepath, index 1 is the extension
-outputPath = ['Calibrated/calibrated_', '.png']
+    #File to load calibration data from
+    paramFile = cfg.get("parameter_file", 'calibrationParams.npz')
+    outputdir = cfg.get("outputdir", '')
+    outputprefix = cfg.get("output_prefix", 'calibrated_')
+    filetype = cfg.get("filetype", '.png')
+
+    
 
 
-# Load calibration parameters from .npz file
-data = np.load(paramFile)
-if data is None:
-    print("Couldn't open file")
-mtx = data['mtx']
-print(mtx)
-dist = data['dist']
-calib_size = tuple(data['calib_size'])  # (width, height)
+    #File to save calibrated image to.
+    #Index 0 is the filepath, index 1 is the extension
+    outputPath = [f'{outputdir}{outputprefix}', filetype]
 
-filepath = input("Enter an image to open: ").strip()
-img = cv.imread(filepath)
-if img is None:
-	print("Couldn't load image")
-	exit()
 
-h,  w = img.shape[:2]
+    # Load calibration parameters from .npz file
 
-# If test image resolution is different from calibration resolution, scale camera matrix
-if (w, h) != calib_size:
-    scale_x = w / float(calib_size[0])
-    scale_y = h / float(calib_size[1])
-    print("Scaling calibration parameters from", calib_size, "to", (w, h))
-    mtx[0, 0] *= scale_x
-    mtx[0, 2] *= scale_x
-    mtx[1, 1] *= scale_y
-    mtx[1, 2] *= scale_y
+    if not os.path.isfile(paramFile):
+        ERR.raiseError("03001", "Couldn't load undistortion parameters", False)
+        return None
+    elif paramFile[-4:] != '.npz':
+        ERR.raiseError("03004", "Undistortion parameter file is of incorrect type.", False)
+        return None
 
-# Get new camera matrix and undistort image
-newcameramtx, roi = cv.getOptimalNewCameraMatrix(mtx, dist, (w,h), 1, (w,h))
-dst = cv.undistort(img, mtx, dist, None, newcameramtx)
 
-# Full undistotred image
-filename = filepath.split("/")[-1]
-output_file = f'{outputPath[0]}{filename.rsplit(".", 1)[0]}{outputPath[1]}'
-cv.imwrite(output_file, dst)
-print(f"Wrote image out to {output_file}")
+    data = np.load(paramFile)
+    mtx = data['mtx']
+    dist = data['dist']
+    calib_size = tuple(data['calib_size'])  # (width, height)
+
+    filepath = filepath.strip()
+
+    img = cv.imread(filepath)
+    
+    if img is None:
+        ERR.raiseError("03002", "Couldn't load input image to undistort", False)
+        return None
+
+    h,  w = img.shape[:2]
+
+    # If test image resolution is different from calibration resolution, scale camera matrix
+    if (w, h) != calib_size:
+        scale_x = w / float(calib_size[0])
+        scale_y = h / float(calib_size[1])
+        print("Scaling calibration parameters from", calib_size, "to", (w, h))
+        mtx[0, 0] *= scale_x
+        mtx[0, 2] *= scale_x
+        mtx[1, 1] *= scale_y
+        mtx[1, 2] *= scale_y
+
+    # Get new camera matrix and undistort image
+    newcameramtx, roi = cv.getOptimalNewCameraMatrix(mtx, dist, (w,h), 1, (w,h))
+    dst = cv.undistort(img, mtx, dist, None, newcameramtx)
+
+    os.makedirs(outputdir, exist_ok=True)
+
+
+    # Full undistotred image
+    filename = filepath.split("/")[-1]
+    print(f"filename is {filename}")
+    output_file = f'{outputPath[0]}{filename.rsplit(".", 1)[0]}{outputPath[1]}'
+    print(f"outputfile is {output_file}")
+    try:
+        cv.imwrite(output_file, dst)
+    except Exception as e:
+        ERR.raiseError("03003", f"Couldn't save image: {e}", False)
+        return None
+
+    return output_file
+
