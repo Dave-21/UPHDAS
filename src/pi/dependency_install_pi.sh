c6eee01f8f0df7c2c7126938607cb4e2e3d15b05
dudeman36012 <117118887+dudeman36012@users.noreply.github.com>
2025-04-04 12:40:43 -0400

    Added most of the python libraries I could find. Not done yet.

--- src/pi/dependency_install_pi.sh
+++ src/pi/dependency_install_pi.sh
@@ -2,13 +2,25 @@
 # Install dependencies on the Raspberry Pi
 
 echo "Updating package lists..."
-sudo apt update
+sudo apt update && sudo apt upgrade
+
+
 
 echo "Installing Python3, pip, and git..."
 sudo apt install -y python3 python3-pip git
 
+#Needs testing
 echo "Installing Python packages..."
-#using pip3 for installation doesn't work on linux, try install python3-[package]
-pip3 install -r ../../requirements.txt
+sudo apt install python3-opencv
+sudo apt install python3-yaml
+sudo apt install python3-numpy
+sudo apt install python3-libcamera
+pip install spacetrack --break-system-packages
+pip install astropy --break-system-packages
+pip install picamera2 --break-system-packages
+pip install skyfield --break-system-packages
+pip install requests --break-system-packages
+
+#figure out how to install argparse/dateutil (or see if they are standard packages)
 
 echo "Pi dependencies installed."
